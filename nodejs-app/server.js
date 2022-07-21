e4562fe11dc3713d4b0479df2e65eb2287bf2b40
Student User <student@workstation.lab.example.com>
2022-07-21 05:22:12 -0400

    fix server.js again

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,13 +1,13 @@
-var createError = require('html-error');
+var createError = require('html-errors');
 
 var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+	  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+	  console.log('Example app listening on port 8080!');
 });
 
